a16053abe7a2502dd840231d90690bd820f67edc
Soumili Bhattacharya <soumilibhattacharya20@gmail.com>
2024-05-20 23:39:44 +0530

    22nd commit

--- app/src/main/java/com/example/examapplication/StudentGroup.java
+++ app/src/main/java/com/example/examapplication/StudentGroup.java
@@ -1,5 +1,7 @@
 package com.example.examapplication;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,6 +9,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -24,22 +30,40 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
-public class StudentGroup extends AppCompatActivity {
+public class StudentGroup extends AppCompatActivity implements TextToSpeech.OnInitListener {
     TextView SGTSHP,SG_GroupDetails,SG_Text,SG_NoText;
     ListView SG_LV;
     ProgressBar SG_P;
     String GROUP_ID;
+    String Rl;
 
     FirebaseAuth authProfile;
+    TextToSpeech textToSpeech;//1
+
+    Handler handler;
+    Runnable toastRunnable;
+
+    boolean isUserInteracted;
+
+    // Flag to indicate if TextToSpeech engine is initialized
+    boolean isTTSInitialized;//1
+
 
     FirebaseUser firebaseUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_group);
+
+        Intent checkIntent = new Intent();//0
+        checkIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkIntent, 1);//0
+
         Intent intent=getIntent();
         GROUP_ID=intent.getStringExtra("GROUP_ID");
+        Rl= intent.getStringExtra("Rl");
         SGTSHP=findViewById(R.id.SGTSHP);
         SG_GroupDetails=findViewById(R.id.SG_GroupDetails);
         SG_Text=findViewById(R.id.SG_Text);
@@ -72,6 +96,202 @@ public class StudentGroup extends AppCompatActivity {
 
         SG_P.setVisibility(View.VISIBLE);
         PopulateList();
+        handler = new Handler();//2
+
+        isUserInteracted = false;
+        isTTSInitialized = false;
+
+        toastRunnable = new Runnable() {
+            @Override
+            public void run() {
+                if(Rl.equals("Student"))
+                    Repeat();
+            }
+        };
+
+        // Start the initial delay
+        startToastTimer();//2
+    }
+    protected void onResume() {
+
+        super.onResume();
+        // Reset the timer whenever the user interacts with the app
+        resetToastTimer();
+        isUserInteracted = false; // Reset user interaction flag
+
+    }
+
+
+    // Method to start the Toast timer
+    private void startToastTimer() {
+
+        handler.postDelayed(toastRunnable, 30000); // 1 minute delay
+
+    }
+
+    // Method to reset the Toast timer
+    private void resetToastTimer() {
+        handler.removeCallbacks(toastRunnable);
+        startToastTimer();
+    }
+
+    private void pauseToastTimer() {
+        handler.removeCallbacks(toastRunnable);
+
+    }
+
+    // Callback when TTS engine finishes speaking
+    UtteranceProgressListener utteranceProgressListener=new UtteranceProgressListener() {
+
+        @Override
+        public void onStart(String utteranceId) {
+            Log.d(TAG, "onStart ( utteranceId :"+utteranceId+" ) ");
+        }
+
+        @Override
+        public void onError(String utteranceId) {
+            Log.d(TAG, "onError ( utteranceId :"+utteranceId+" ) ");
+        }
+
+        @Override
+        public void onDone(String utteranceId) {
+            resetToastTimer();
+        }
+    };
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(Rl.equals("Student")) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == 1) {
+                if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                    // TTS engine is available, initialize TextToSpeech
+                    textToSpeech = new TextToSpeech(this, this);
+                    textToSpeech.setOnUtteranceProgressListener(utteranceProgressListener);
+                } else {
+                    // TTS engine is not installed, prompt the user to install it
+                    Intent installIntent = new Intent();
+                    installIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                    startActivity(installIntent);
+                }
+            }
+        }
+    }
+
+
+
+    @Override
+    public void onInit(int status) {
+        if(Rl.equals("Student")) {
+            if (status == TextToSpeech.SUCCESS) {
+                // TTS initialization successful, set language and convert text to speech
+                isTTSInitialized = true;
+                textToSpeech.setLanguage(Locale.US);
+                //Locale locale = new Locale("en","IN");
+                //Name: en-in-x-end-network Locale: en_IN Is Network TTS: true
+                //Voice voice = new Voice("en-in-x-end-network", locale, 400, 200, true, null); // Example voice
+                //textToSpeech.setVoice(voice);
+                int ttsResult = textToSpeech.speak("Hello, Welcome to the Student Group Page of Exam Care, This page provides you with the facility, to " +
+                        "                \" know about your assignments which you can attempt, you just have to say, Hello Exam care, StudentGroup .", TextToSpeech.QUEUE_FLUSH, null, "TTS_UTTERANCE_ID");
+                if (ttsResult == TextToSpeech.SUCCESS) {
+                    // Pause the timer until TTS completes
+                    pauseToastTimer();
+                }
+            } else {
+                // TTS initialization failed, handle error
+                Log.e("TTS", "Initialization failed");
+            }
+        }
+    }
+
+    // Repeat The Introduction if Repeat Method is Triggered.
+    public void StarUpRepeat(){
+
+        resetToastTimer();
+        textToSpeech.setLanguage(Locale.US);
+        //Locale locale = new Locale("en","IN");
+        //Name: en-in-x-end-network Locale: en_IN Is Network TTS: true
+        //Voice voice = new Voice("en-in-x-end-network", locale, 400, 200, true, null); // Example voice
+        //textToSpeech.setVoice(voice);
+        int ttsResult = textToSpeech.speak("Hello, Welcome to the Profile Page of Exam Care, This page provides you with the facility, to " +
+                "see your profile details such as name, email, phone no, institute, username and role, you just have to say, Hello Exam care, profile details.", TextToSpeech.QUEUE_FLUSH, null, "TTS_UTTERANCE_ID");
+        if (ttsResult == TextToSpeech.SUCCESS) {
+            // Pause the timer until TTS completes
+            pauseToastTimer();
+        }
+        Repeat();
+
+    }
+
+    public void Repeat(){
+        textToSpeech.setLanguage(Locale.US);
+        //Locale locale = new Locale("en","IN");
+        //Name: en-in-x-end-network Locale: en_IN Is Network TTS: true
+        //Voice voice = new Voice("en-in-x-end-network", locale, 400, 200, true, null); // Example voice
+        //textToSpeech.setVoice(voice);
+        int ttsResult = textToSpeech.speak("If you want me to repeat the introduction of the page again please say, Exam Care Repeat Introduction", TextToSpeech.QUEUE_FLUSH, null, "TTS_UTTERANCE_ID");
+        if (ttsResult == TextToSpeech.SUCCESS) {
+            // Pause the timer until TTS completes
+            pauseToastTimer();
+        }
+        //Enter the Condition Over here that is tts to take input from the user if they wants us to repeat the introduction and change r respectively.
+        boolean r = false;
+        if (r == true) {
+            StarUpRepeat();
+        }
+
+    }
+
+
+
+    @Override
+    protected void onDestroy() {
+
+        // Release resources
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+
+        super.onDestroy();
+        handler.removeCallbacks(toastRunnable);
+
+
+    }//3
+
+    public void VoiceLogin(){
+
+        textToSpeech.setLanguage(Locale.US);
+        //Locale locale = new Locale("en","IN");
+        //Name: en-in-x-end-network Locale: en_IN Is Network TTS: true
+        //Voice voice = new Voice("en-in-x-end-network", locale, 400, 200, true, null); // Example voice
+        //textToSpeech.setVoice(voice);
+        int tts1=textToSpeech.speak("Let's, Begin the login Process.", TextToSpeech.QUEUE_FLUSH, null,"TTS_UTTERANCE_ID");
+        if (tts1 == TextToSpeech.SUCCESS) {
+            // Pause the timer until TTS completes
+            pauseToastTimer();
+        }
+        int tts2=textToSpeech.speak("Please Say, Exam Care and then your Email ID", TextToSpeech.QUEUE_FLUSH, null,"TTS_UTTERANCE_ID");
+        if (tts2 == TextToSpeech.SUCCESS) {
+            // Pause the timer until TTS completes
+            pauseToastTimer();
+        }
+        String Email=""; // Store the Email over here using STT.
+        int tts3=textToSpeech.speak("Please Say, Exam Care and then your Password", TextToSpeech.QUEUE_FLUSH, null,"TTS_UTTERANCE_ID");
+        if (tts3 == TextToSpeech.SUCCESS) {
+            // Pause the timer until TTS completes
+            pauseToastTimer();
+        }
+        String pwd=""; //Store Email over here using STT.
+
+        int tts4=textToSpeech.speak("Please Say, Exam Care Log me In, Inorder to login", TextToSpeech.QUEUE_FLUSH, null,"TTS_UTTERANCE_ID");
+        if (tts4 == TextToSpeech.SUCCESS) {
+            // Pause the timer until TTS completes
+            pauseToastTimer();
+        }
+        boolean profile=false;//Edit This Using STT
+        if ( profile== true) {
+            //loginUser(Email,pwd);
+        }
     }
 
     public void PopulateList() {
